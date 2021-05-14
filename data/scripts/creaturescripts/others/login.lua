0634b5ad16eabcbb172523bb13b9487544503e8e
Tanner Bruner <tannerbruner@gmail.com>
2021-05-13 19:02:11 -0500

    Updated login.lua to include a script to broadcast a message when a player logs into the game.

--- data/scripts/creaturescripts/others/login.lua
+++ data/scripts/creaturescripts/others/login.lua
@@ -37,6 +37,7 @@ function playerLogin.onLogin(player)
 		db.query('UPDATE `players` SET `istutorial` = 0 where `id`='..player:getGuid())
 	else
 		player:sendTextMessage(MESSAGE_STATUS, "Welcome to " .. SERVER_NAME .. "!")
+		Game.broadcastMessage('You feel a shift as ' .. player:getName() .. ' joins the world...', MESSAGE_EVENT_ADVANCE)
 		player:sendTextMessage(MESSAGE_LOGIN, string.format("Your last visit in ".. SERVER_NAME ..": %s.", os.date("%d. %b %Y %X", player:getLastLoginSaved())))
 	end
 
