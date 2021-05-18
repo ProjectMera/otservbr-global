1a88038c4aa371c429f92feb08314b80d073c274
BuildTools <unconfigured@null.spigotmc.org>
2021-05-17 17:47:49 -0700

    Fixed console error for coins script Updated to give 1 coin every 15 minutes

--- data/events/scripts/scheduler/coinevent.lua
+++ data/events/scripts/scheduler/coinevent.lua
@@ -1,26 +1,24 @@
-local CoinEvent = GlobalEvent("Coin Event")
-function CoinEvent.onStartup()
+local CoinDistribute = GlobalEvent("Coin Distribute")
+
+function CoinDistribute.onThink(interval)
+	local players = Game.getPlayers()
+	if next(players) == nil then
+		return true;
+	end
+	local amount = tonumber(1)
+	for _, targetPlayer in ipairs(players) do
+		if targetPlayer ~= nil then
+			local accountId = getAccountNumberByPlayerName(targetPlayer)
+			if accountId ~= 0 then 
+				local result = db.query('UPDATE accounts SET coins = coins+'.. amount ..' WHERE id = ' .. accountId)
+				if result then
+					targetPlayer:sendTextMessage(MESSAGE_INFO_DESCR, "You received " .. amount .. " Mera coins.")
+				end
+			end
+		end
+	end
 	return true
 end
 
-CoinEvent:register()
-
-local CoinDistribute = GlobalEvent("Coin Distribute")
-function CoinDistribute.onThink(interval)
-	local players = Game.getPlayers()
-	local amount = tonumber(1)
-	for _, targetPlayer in ipairs(players) do
-		local accountId = getAccountNumberByPlayerName(targetPlayer)
-    if accountId == 0 then
-        return true
-    end
-    local result = db.query('UPDATE accounts SET coins = coins+'.. amount ..' WHERE id = ' .. accountId)
-    if result then
-        targetPlayer:sendTextMessage(MESSAGE_INFO_DESCR, "You received " .. amount .. " Mera coins.")
-    end
-	return true
-	end
-	end
-
-CoinDistribute:interval(1000 * 60 * 60) -- 1 Hour interval
+CoinDistribute:interval(1000 * 60 * 15) -- 15 minute interval
 CoinDistribute:register()
